fe75d7fb548fca568b136b75d5caa749c0b293df
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-05-18 01:02:41 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # notes
+
+## Class-01
+
+#### UML
+
+![class01](./assets/class-01.png)
---
+++ index.js
@@ -0,0 +1,10 @@
+const Input = require('./lib/input.js');
+const Notes = require('./lib/notes.js');
+
+
+let input = new Input()
+let notes = new Notes()
+notes.execute(input)
+
+
+
